e925e7f88b227ab20135aa01eac1df6085aeef7c
wadieskaf <wadie.skaf@live.com>
2020-03-27 16:49:16 +0100

    A new version of MapHandler

---
+++ javaagents/src/main/java/massim/javaagents/IntegerPair.java
@@ -0,0 +1,20 @@
+package massim.javaagents;
+
+public class IntegerPair {
+    private int x;
+    private int y;
+
+    public IntegerPair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
---
+++ javaagents/src/main/java/massim/javaagents/MapHandler.java
@@ -0,0 +1,79 @@
+package massim.javaagents;
+
+import massim.javaagents.utils.Block;
+import massim.javaagents.utils.Thing;
+
+import java.util.List;
+
+public class MapHandler {
+    private int[][] map;
+    private IntegerPair agentLocation;
+
+    public int[][] getMap() {
+        return map;
+    }
+
+    public void initiateMap(int length, int width, IntegerPair agentIntegerPair) {
+        this.map = new int[length][width];
+        this.agentLocation = agentIntegerPair;
+        int x = agentIntegerPair.getX();
+        int y = agentIntegerPair.getY();
+        //Agent location has value of 7
+        map[x][y] = 7;
+    }
+
+    private void moveAgent(IntegerPair agentMovement) {
+        int new_x = this.agentLocation.getX() - agentMovement.getX();
+        int new_y = this.agentLocation.getY() + agentMovement.getY();
+        this.agentLocation = new IntegerPair(new_x, new_y);
+    }
+
+    public void updateMap(PerceptionHandler handler, IntegerPair agentMovement) {
+        this.map[agentLocation.getX()][agentLocation.getY()] = 0;
+        this.moveAgent(agentMovement);
+        this.map[agentLocation.getX()][agentLocation.getY()] = 7;
+        List<Thing> enemies = handler.getEnemies();
+        List<Thing> teammates = handler.getTeammates();
+        List<Thing> obstacles = handler.getObstacles();
+        List<Thing> goals = handler.getGoals();
+        List<Block> blocks = handler.getBlocks();
+        List<Block> dispensers = handler.getDispensers();
+
+        int agent_x = agentMovement.getX();
+        int agent_y = agentMovement.getY();
+        int x;
+        int y;
+
+        // Enemies have value 1
+        for (var enemy : enemies) {
+            x = enemy.getX();
+            y = enemy.getY();
+            this.map[agent_x - x][agent_y + y] = 1;
+        }
+        // Teammates have value 2
+        for (var teammate : teammates) {
+            x = teammate.getX();
+            y = teammate.getY();
+            this.map[agent_x - x][agent_y + y] = 2;
+        }
+        // Obstacles have value 3
+        for (var obstacle : obstacles) {
+            x = obstacle.getX();
+            y = obstacle.getY();
+            this.map[agent_x - x][agent_y + y] = 3;
+        }
+        // Goals have value 4
+        for (var goal : goals) {
+            x = goal.getX();
+            y = goal.getY();
+            this.map[agent_x - x][agent_y + y] = 4;
+        }
+
+        // Dispensers have value 5
+        for (var dispenser : dispensers) {
+            x = dispenser.getX();
+            y = dispenser.getY();
+            this.map[agent_x + x][agent_y - y] = 5;
+        }
+    }
+}

03af505774238eeb6e9aa4c3d4264e0270a1a4b4
BrunoMelicio <brunomelicio17@gmail.com>
2020-03-29 10:58:06 +0200

    New search options

--- javaagents/src/main/java/massim/javaagents/BFSsearch.java
+++ javaagents/src/main/java/massim/javaagents/BFSsearch.java
@@ -1,8 +1,10 @@
 package massim.javaagents;
 
+import java.sql.SQLOutput;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import massim.javaagents.utils.*;
 
 public class BFSsearch {
     MapHandler mh;
@@ -15,10 +17,11 @@ public class BFSsearch {
     boolean reached_goal;
     Queue<IntegerPair> queue;
     int[][] map; //keep track of visited cells
-    CellType cellType;
     List <IntegerPair> path;
+    int length;
+    int width;
 
-    public BFSsearch(MapHandler mh, int length, int width,CellType ct){
+    public BFSsearch(MapHandler mh, int length, int width){
         this.mh = mh;
         this.agentCurrentPos = this.mh.getAgentLocation();
         this.move_count = 0;
@@ -26,19 +29,24 @@ public class BFSsearch {
         this.cells_in_next_layer = 0;
         this.reached_goal = false;
         this.queue = new LinkedList<>();
+        this.length = length;
+        this.width = width;
         this.map = new int[length][width];
-        this.cellType = ct;
+        this.path = new LinkedList<>();
     }
 
-    public List<IntegerPair> solve(){
-        //solve
+
+    public List<IntegerPair> bfsByLocation(IntegerPair goal){
+        //System.out.println("Starting Position: (" + agentCurrentPos.getX() + "," + agentCurrentPos.getY() + ")");
         queue.add(agentCurrentPos);
-        map[agentCurrentPos.getX()][agentCurrentPos.getY()] = 1; //mark as visited
+        this.map[agentCurrentPos.getX()][agentCurrentPos.getY()] = 1; //mark as visited
 
         while (queue.size() > 0){
 
             IntegerPair currentCell = queue.remove();
-            if(mh.getMap()[currentCell.getX()][currentCell.getY()] == new OrdinaryCell(this.cellType)){
+            //System.out.println("Current: (" + currentCell.getX() + "," + currentCell.getY() + ")");
+            if(currentCell.getX() == goal.getX() && currentCell.getY() == goal.getY()){
+                path.add(currentCell);
                 reached_goal = true;
                 break;
             }
@@ -54,10 +62,48 @@ public class BFSsearch {
             }
         }
 
-        if(reached_goal){
+        /*if(reached_goal){
             return path;
+        }*/
+        return path;
+    }
+
+    public List<IntegerPair> bfsByType(CellType cellType, String detail){
+        //System.out.println("Starting Position: (" + agentCurrentPos.getX() + "," + agentCurrentPos.getY() + ")");
+        queue.add(agentCurrentPos);
+        this.map[agentCurrentPos.getX()][agentCurrentPos.getY()] = 1; //mark as visited
+
+        while (queue.size() > 0){
+
+            IntegerPair currentCell = queue.remove();
+            //System.out.println("Current: (" + currentCell.getX() + "," + currentCell.getY() + ")");
+            if(mh.getMap()[currentCell.getX()][currentCell.getY()] == new OrdinaryCell(cellType)){
+                if(detail==""){
+                    path.add(currentCell);
+                    reached_goal = true;
+                    break;
+                }
+                else{
+                    //check if cell detail is the same as the preferred detail
+                    //this applies to blocks and dispensers in which detail is the type -- !!!!!! WAITING METHOD !!!!!!!
+                }
+            }
+            //if not goal cell -> explore neighbours
+            explore_neighbours(currentCell);
+
+            cells_left_in_layer -= 1;
+            if (cells_left_in_layer == 0){
+                cells_left_in_layer = cells_in_next_layer;
+                cells_in_next_layer = 0;
+                move_count +=1;
+                path.add(currentCell);//not sure if this is correct
+            }
         }
-        return null;
+
+        /*if(reached_goal){
+            return path;
+        }*/
+        return path;
     }
 
     public void explore_neighbours(IntegerPair current){
@@ -74,13 +120,17 @@ public class BFSsearch {
             int newPosX = current.getX() + directionInX[i];
             int newPosY = current.getY() + directionInY[i];
             newCellPos =  new IntegerPair(newPosX, newPosY);
+            //System.out.println("New CellPos: (" + newCellPos.getX() + "," + newCellPos.getY() + ")");
 
+            if(newCellPos.getX() < 0 || newCellPos.getX() > this.length - 1 || newCellPos.getY() < 0 || newCellPos.getY() > this.width -1){
+                continue;
+            }
             //check if position has already been visited
             if(this.map[newCellPos.getX()][newCellPos.getY()] == 1) {
                 continue;
             }
             //check if the newPosition is valid to move
-            if(mh.getMap()[newCellPos.getX()][newCellPos.getY()] == new OrdinaryCell(CellType.Obstacle) || mh.getMap()[newCellPos.getX()][newCellPos.getY()] == new OrdinaryCell(CellType.Obstacle)){
+            if(mh.getMap()[newCellPos.getX()][newCellPos.getY()] == new OrdinaryCell(CellType.Obstacle) || mh.getMap()[newCellPos.getX()][newCellPos.getY()] == new OrdinaryCell(CellType.Unknown)){
                 continue; //then check other direction
             }
 

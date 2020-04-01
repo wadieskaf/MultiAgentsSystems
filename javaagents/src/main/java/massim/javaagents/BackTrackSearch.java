2a2aa2ac4eb512cdb27a81f36f1443a30bf33b51
wadieskaf <wadie.skaf@live.com>
2020-04-01 11:11:51 +0200

    BackTrackSearch class added.

---
+++ javaagents/src/main/java/massim/javaagents/BackTrackSearch.java
@@ -0,0 +1,148 @@
+package massim.javaagents;
+
+import massim.javaagents.utils.Cell;
+import massim.javaagents.utils.CellType;
+import massim.javaagents.utils.IntegerPair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BackTrackSearch {
+    private List<IntegerPair> visitedNodes;
+    private IntegerPair currentAgentLocation;
+    private IntegerPair initialAgentLocation;
+    private List<CellType> stuckTypes;
+    private List<IntegerPair> path;
+    private int agentVision;
+    private List<IntegerPair> visionBorders;
+    List<IntegerPair> possibleMovementsCoordinates;
+
+    public BackTrackSearch(MapHandler mapHandler, int agentVision){
+        this.visitedNodes = new ArrayList<>();
+        this.currentAgentLocation = this.initialAgentLocation = mapHandler.getAgentLocation();
+        this.stuckTypes = new ArrayList<>(
+                List.of(CellType.Obstacle)
+        );
+        this.path = new ArrayList<>();
+        this.visitedNodes.add(currentAgentLocation);
+        this.agentVision = agentVision;
+        this.visionBorders = calculateVisionBorders();
+        this.possibleMovementsCoordinates = possibleMovementsCoordinates();
+    }
+
+    public IntegerPair getBestMovement(){
+        if (this.path.size() == 0){
+            return null;
+        }
+        else {
+            return this.path.get(0);
+        }
+    }
+/*    private List<IntegerPair> findNearbyUnknow(){
+        List<IntegerPair> nearbyLocations = new ArrayList<>();
+        List<Integer> possibleMovements = new ArrayList<>(
+                List.of(1,-1)
+        );
+        Cell[][] map = this.mapHandler.getMap();
+        for (int i: possibleMovements){
+            int x = mapHandler.getAgentLocation().getX() + i;
+            int y = mapHandler.getAgentLocation().getY();
+            if (map[x][y].getType() == CellType.Unknown){
+                nearbyLocations.add(new IntegerPair(x,y));
+            }
+            x = mapHandler.getAgentLocation().getX();
+            y = mapHandler.getAgentLocation().getY() + i;
+            if (map[x][y].getType() == CellType.Unknown){
+                nearbyLocations.add(new IntegerPair(x,y));
+            }
+        }
+        return nearbyLocations;
+    }*/
+    private List<IntegerPair> possibleMovementsCoordinates(){
+        List<IntegerPair> result = new ArrayList<>();
+        List<Integer> possibleMovements = new ArrayList<>(
+                List.of(1,-1)
+        );
+        for (int i: possibleMovements){
+            result.add(new IntegerPair(i,0));
+            result.add(new IntegerPair(0,i));
+        }
+        return result;
+    }
+    private Boolean checkMovement(IntegerPair movement){
+        IntegerPair newLocation = this.currentAgentLocation.add(movement);
+        return newLocation.getX() < 100 && newLocation.getX() >= 0 &&
+                newLocation.getY() < 100 && newLocation.getY() >= 0;
+    }
+    private int calculateDistance(){
+        return (Math.abs(this.initialAgentLocation.getX() - this.currentAgentLocation.getX()) +
+                Math.abs(this.initialAgentLocation.getY() - this.currentAgentLocation.getY()))/2;
+    }
+    private List<IntegerPair> calculateVisionBorders(){
+        List<IntegerPair> visionBorders = new ArrayList<>();
+        IntegerPair right = this.initialAgentLocation.add(new IntegerPair(0, this.agentVision));
+        IntegerPair top = this.initialAgentLocation.add((new IntegerPair(this.agentVision, 0)));
+        IntegerPair left = this.initialAgentLocation.add((new IntegerPair(0, -1 * this.agentVision)));
+        IntegerPair bottom = this.initialAgentLocation.add(new IntegerPair(-1 * this.agentVision, 0));
+        visionBorders.add(right);
+        visionBorders.add(left);
+        visionBorders.add(top);
+        visionBorders.add(bottom);
+        for (int i = 1; i<this.agentVision;i++){
+            visionBorders.add(left.add(new IntegerPair(i,i)));
+            visionBorders.add(left.add(new IntegerPair(-i,i)));
+            visionBorders.add(right.add(new IntegerPair(i,-i)));
+            visionBorders.add(right.add(new IntegerPair(-i,-i)));
+        }
+        return visionBorders;
+    }
+    public Boolean search(MapHandler mapHandler){
+        List<IntegerPair> possibleMovements = new ArrayList<>();
+        for (IntegerPair movement:this.possibleMovementsCoordinates){
+            if (checkMovement(movement)){
+                possibleMovements.add(movement);
+            }
+        }
+        if (possibleMovements.size() == 0){
+            return false;
+        }
+        if (this.visionBorders.contains(this.currentAgentLocation)) {
+            for(IntegerPair movement:possibleMovements){
+                if (mapHandler.getCell(currentAgentLocation.add(movement)).getType() == CellType.Unknown){
+                    return true;
+                }
+                return false;
+            }
+        }
+        int stuckCount=0;
+
+        for (IntegerPair i: possibleMovements) {
+            Cell cell = mapHandler.getCell(currentAgentLocation.add(i));
+            if (stuckTypes.contains(cell.getType()) || visitedNodes.contains(currentAgentLocation.add(i))) {
+                ++stuckCount;
+            }
+        }
+        if (stuckCount == possibleMovements.size()){
+            return false;
+        }
+        for (IntegerPair movement:possibleMovements){
+            if (this.visitedNodes.contains(currentAgentLocation.add(movement))){
+                continue;
+            }
+            this.path.add(movement);
+            this.visitedNodes.add(currentAgentLocation.add(movement));
+            this.currentAgentLocation = this.currentAgentLocation.add(movement);
+           mapHandler.moveAgent(movement);
+            if (search(mapHandler)){
+                return true;
+            }
+            this.path.remove(path.size() - 1);
+            this.currentAgentLocation = this.currentAgentLocation.add(movement.inverse());
+            mapHandler.moveAgent(movement.inverse());
+
+        }
+        return false;
+
+    }
+
+}
